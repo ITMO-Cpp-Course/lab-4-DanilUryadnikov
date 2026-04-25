873563b62c37d81c97f104890bfcce4915340dcc
Danil <uryadnikov.07@mail.ru>
2026-04-25 23:24:36 +0300

    itog1

--- src/resource_core/src/resource_manager.cpp
+++ src/resource_core/src/resource_manager.cpp
@@ -1,23 +1,29 @@
 #include "lab4/resource/resource_manager.hpp"
 #include <memory>
 
-namespace lab4::resource{
-    std::shared_ptr<FileHandle> ResourceManager::getFile(const std::string& filename, FileHandle::Mode mode){
-        auto it = cache_.find(filename);
-        if(it != cache_.end()){
-            auto ptr = it->second.lock();
-            if(ptr){
-                return ptr;
-            }
-            else{
-                cache_.erase(it);
-            }
+namespace lab4::resource
+{
+std::shared_ptr<FileHandle> ResourceManager::getFile(const std::string& filename, FileHandle::Mode mode)
+{
+    auto it = cache_.find(filename);
+    if (it != cache_.end())
+    {
+        auto ptr = it->second.lock();
+        if (ptr)
+        {
+            return ptr;
         }
-    auto ptr = std::make_shared<FileHandle>(filename,mode);
+        else
+        {
+            cache_.erase(it);
+        }
+    }
+    auto ptr = std::make_shared<FileHandle>(filename, mode);
     cache_[filename] = ptr;
     return ptr;
-    }
-    void ResourceManager::removeFromCache(const std::string& filename) {
+}
+void ResourceManager::removeFromCache(const std::string& filename)
+{
     cache_.erase(filename);
 }
-}
+} // namespace lab4::resource
